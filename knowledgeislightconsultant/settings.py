142f420792326c07b97218c826d1f7743c2947d3
Jonathan Pollyn <j.pollyn@gmail.com>
2020-03-04 04:57:04 -0700

    Removed Email information from settingst

--- knowledgeislightconsultant/settings.py
+++ knowledgeislightconsultant/settings.py
@@ -137,11 +137,7 @@ MESSAGE_TAGS = {
 } 
 
 # Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='djangoemail321@gmail.com'
-EMAIL_HOST_PASSWORD='djangoemail123'
-EMAIL_USE_TLS=True 
+
 
 try:
     from .local_settings import *
